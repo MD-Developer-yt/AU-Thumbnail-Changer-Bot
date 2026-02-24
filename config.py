832b6e7d1f5d49ed7dd8d3a7c5069f04b95b2354
MD-Developer-yt <animeworldday2005@gmail.com>
2026-02-24 15:26:04 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import random
 # Bot Configuration
-API_TOKEN = os.environ.get("API_TOKEN", "23639272")
+API_TOKEN = os.environ.get("API_TOKEN", "8518273836:AAFrIAg5h83ldwocyfSO6Ht0Jukk3GaH3v0")
 # MongoDB
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://amimeinhindi:n2PMMs07YTC1Unpg@cluster0.l6wffte.mongodb.net/?appName=Cluster0")
 DB_NAME = "thumbnail_bot"

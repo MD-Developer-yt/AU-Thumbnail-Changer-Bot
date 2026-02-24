0bf7d4272760987fe5c45101f835ad055edf52f8
MD-Developer-yt <animeworldday2005@gmail.com>
2026-02-24 20:47:32 +0530

    config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 import random
 # Bot Configuration
-API_TOKEN = os.environ.get("API_TOKEN", "8518273836:AAFrIAg5h83ldwocyfSO6Ht0Jukk3GaH3v0")
+API_TOKEN = os.environ.get("API_TOKEN", "8518273836:AAFrIAg5h83ldwocyfSO6Ht0Jukk3GaH3v0") #add your bot token
 # MongoDB
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://amimeinhindi:n2PMMs07YTC1Unpg@cluster0.l6wffte.mongodb.net/?appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://amimeinhindi:n2PMMs07YTC1Unpg@cluster0.l6wffte.mongodb.net/?appName=Cluster0") #add your MongoDB URL
 DB_NAME = "thumbnail_bot"
 # Owner/Admin
-OWNER_ID = int(os.environ.get("OWNER_ID", "7284759394"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7284759394")) #add your User id
 # UI URLs - Multiple images that rotate randomly
 # Use DIRECT image URLs (https://i.ibb.co/...) not page URLs (https://ibb.co/...)
 START_PICS = [
@@ -22,3 +22,7 @@ def get_random_pic() -> str:
     if START_PICS:
         return random.choice(START_PICS)
     return None
+#Don't Remove Credits 
+#Supports Group @AU_Bot_Discussion 
+#Telegram Channel @Anime_UpdatesAU
+#Developer @Mr_Mohammed_29
